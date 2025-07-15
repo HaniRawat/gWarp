300388e38ecbfc6a73b8fc901a7cdbb83d2aaa36
HaniRawat <120402194+HaniRawat@users.noreply.github.com>
2025-07-15 22:55:58 +0530

    feat: space achieved

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
+'use client'
 import React from 'react'
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Stars } from '@react-three/drei';
 
 const page = () => {
   return (
     <div className='w-full h-screen flex justify-center items-center'>
-      hopefully 3d mesh and stars using three.js and react-three-fiber here
+      <div className="absolute inset-0">
+        <Canvas camera={{ position: [0, 0, 15], fov: 60 }}>
+          <color attach="background" args={['#000011']} />
+          <ambientLight intensity={0.4} />
+          <pointLight position={[10, 10, 10]} intensity={0.5} />
+          <Stars radius={300} depth={80} count={10000} factor={7} saturation={0} />
+          <OrbitControls enableDamping dampingFactor={0.05} />
+          {/* <Scene3D objects={objects} lightRayEnabled={lightRayEnabled} /> */}
+        </Canvas>
+      </div>
       </div>
   )
 }
